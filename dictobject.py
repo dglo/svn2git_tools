06bf5d6668c8eab018ee479a65ea12f605580d5f
Dave Glowacki <dglo@icecube.wisc.edu>
2020-10-05 16:40:17 -0500

    Error message should mention to data type

---
+++ dictobject.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+
+class DictObject(dict):
+    """
+    Generic class which can be used as either a dictionary or an object
+    """
+    def __getattr__(self, name):
+        if name not in self:
+            raise AttributeError("Unknown <%s> attribute \"%s\"" %
+                                 (type(self), name))
+        return self[name]
+
+    def __setattr__(self, name, value):
+        self[name] = value
+
+    def __delattr__(self, name):
+        if name not in self:
+            raise AttributeError("Unknown attribute \"%s\"" % str(name))
+        del self[name]
+
+    def set_value(self, attribute, value):
+        "Set a dictionary value (which also creates an attrbiute)"
+        self[attribute] = value
